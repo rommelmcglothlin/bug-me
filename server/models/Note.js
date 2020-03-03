ab4a6d15d7dd0a62a90c525de8a880a1899e38bb
rommelmcglothlin <rommel.mcglothlin@icloud.com>
2020-03-02 18:10:12 -0700

    9 out of 9 pass

--- server/models/Note.js
+++ server/models/Note.js
@@ -5,7 +5,7 @@ let ObjectId = Schema.Types.ObjectId;
 let note = new Schema(
   {
     content: { type: String, maxlength: 10000, required: true },
-    bugId: { type: ObjectId, ref: "Bug" },
+    bug: { type: ObjectId, ref: "Bug" },
     reportedBy: { type: String, required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
